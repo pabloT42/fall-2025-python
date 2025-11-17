b121d7a9fe2f908e868df014e3099367accbcdd1
pabloT42 <saucysquirrel24@gmail.com>
2025-11-17 09:14:10 -0700

    Create dif param.py

---
+++ dif param.py
@@ -0,0 +1,9 @@
+def birds(a,b,c,d):
+    print (a*b, c*d)
+    
+bird1 = input("enter bird")
+num1 = int(input("enter a number"))
+bird2 = input("enter bird")
+num2 = int(input("enter a number"))
+
+birds(bird1, num1, bird2, num2)
